d277e609b8c2564b770b1f35116a6328548d8fed
krrish2906 <krrishbansal290605@gmail.com>
2026-04-19 11:06:10 +0530

    sqlite integration

---
+++ 02_Chatbot_using_LangGraph/langgraph_backend_sqlite.py
@@ -0,0 +1,50 @@
+from langgraph.graph import StateGraph, START, END
+from langchain_groq import ChatGroq
+from typing import TypedDict, Annotated
+from langgraph.graph.message import add_messages
+from langchain_core.messages import BaseMessage
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+llm = ChatGroq(model="llama-3.3-70b-versatile")
+
+def chat_node(state: ChatState):
+    messages = state["messages"]
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+
+# Get the directory of the current script
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "chatbot.db")
+
+# Connect to SQLite database
+conn = sqlite3.connect(db_path, check_same_thread=False)
+checkpointer = SqliteSaver(conn=conn)
+
+# Create the graph
+graph = StateGraph(ChatState)
+
+
+# Add nodes
+graph.add_node("chat_node", chat_node)
+
+# Add edges
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+# Compile the graph
+chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config["configurable"]["thread_id"])
+    
+    return list(all_threads)
